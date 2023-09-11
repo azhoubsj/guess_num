2910a71b0e26d3a68b3c8b7ddc44f7cb35b65f4f
azhoubsj <138343605+azhoubsj@users.noreply.github.com>
2023-09-11 12:42:50 +0800

    Create guess_the_number_game.py

---
+++ guess_the_number_game.py
@@ -0,0 +1,13 @@
+#guess the number name
+import random
+num = random.randint(1,100)
+while True:
+	numa = input('猜猜数字是什么：')
+	numa = float(numa)
+	if numa == num:
+		print('你猜对啦！')
+		break
+	elif numa > num:
+		print('猜大了一些哟，继续猜')
+	elif numa < num:
+		print('猜小了一些哟，继续猜')
